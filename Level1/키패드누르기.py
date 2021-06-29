7b8f67e7b3dde5c5b0e8dd59cd6772a735a48b34
pikipoko <59064298+pikipoko@users.noreply.github.com>
2021-06-29 15:42:41 +0900

    [카카오 인턴] 키패드 누르기
    abs(int n) - n의 절대값 반환
    이걸 이용하면 distanceCal 함수를 더 간단하게 작성 가능

--- Level1/키패드누르기.py
+++ Level1/키패드누르기.py
@@ -13,7 +13,13 @@ def solution(numbers, hand):
     answer = ''
         
     phone = [[3, 1], [0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2], [3, 0], [3, 2]]
-    
+    """
+    1 2 3   0,0 0,1 0,2
+    4 5 6   1,0 1,1 1,2
+    7 8 9   2,0 2,1 2,2
+    * 0 #   3,0 3,1 3,2
+    numbers의 수를 index로 바로 이용하기 위해 0~9를 좌표로 두었다.
+    """
     left = phone[10]   # 왼손 시작점 *
     right = phone[11]  # 오른손 시작점 #
     for n in numbers:
@@ -23,6 +29,7 @@ def solution(numbers, hand):
         elif n in [3,6,9]:
             answer += "R"
             right = phone[n]
+        # 중앙에 있는 숫자들만 거리계산을 하면 된다. - 2,5,8,0
         elif distanceCal(left, phone[n]) < distanceCal(right, phone[n]):
             answer += "L"
             left = phone[n]
